04976fd9daa0e67d29be039a87fdfd8f3a6c1115
tabiernath <tabiernath@gmail.com>
2020-06-12 15:56:22 -0500

    Work on challenges

--- scripts/troy/20200612_jitter_coords.r
+++ scripts/troy/20200612_jitter_coords.r
@@ -5,7 +5,7 @@ pacman::p_load("tidyverse")
 setwd("C:/Users/tabie/OneDrive/Documents/GitHub/TARA-thiolases")
 
 # Read in your full50 dataset
-
+full50 <- read_csv("data/full50_raw.csv")
 # Credit for these functions goes to:
 # https://github.com/lmullen/mullenMisc
 
@@ -26,21 +26,21 @@ setwd("C:/Users/tabie/OneDrive/Documents/GitHub/TARA-thiolases")
 #' jitter_latlong(42, type = "lat", km = 1)
 #' @export
 #' @seealso \code{\link{length_of_degree}}
-jitter_latlong <- function(coord, type = c("lat", "long"), latitude, km = 1) {
-  type = match.arg(type)
+jitter_latlong <- function(coord, type = c("lat", "long"), latitude, km = 1) { # making a function of coord, type (default c("lat", "long")), latitude, and km (default 1)
+  type = match.arg(type) # allowing inputs for lat and long to be sorted to the correct one even if they don't input exactly "lat" or "long"?
   
   if(missing(latitude) & type == "lat") { 
-    latitude <- coord }
-  
-  km_per_degree <- length_of_degree(latitude, type = type)
-  degree_per_km <- 1 / km_per_degree
+    latitude <- coord } # if there isn't a value given for latitude, but coord is a latitude, then use that
+
+  km_per_degree <- length_of_degree(latitude, type = type) # finding the length of a degree from the function explained below
+  degree_per_km <- 1 / km_per_degree # blah
   coord + (runif(1, min = -1, max = 1) * degree_per_km * km)
-}
+} #^ taking the coord, adding or subtracting a randomized amount, and then scaling it by the length a degree should be and how many km we want to allow it to move
 
 # What is going on here???
 jitter_latlong <- Vectorize(jitter_latlong,
-                            vectorize.args = c("coord", "latitude"))
-
+                           vectorize.args = c("coord", "latitude"))
+#^ turning the coord and latitude into a vector? I don't know--if I comment this out nothing seems different
 
 
 #' Length of a degree of latitude or longitude
@@ -69,7 +69,14 @@ length_of_degree <- function(degree, type = c("lat", "long")) {
 
 # Challenge 1. Walk through the functions and look up any commands you don't know.
 # For example, what is match.arg? runif? Vectorize? 
-
+?match.arg
+  # https://alistaire.rbind.io/blog/match.arg/
+  # checks the input against a set of choices, can partial match
+?runif
+  # seems like it will generate random values in a distribution specified
+?Vectorize
+  # vectorizes the action of its argument
+  # maybe takes all loose values of coord and lat and combines them into a respective vector?
 # Challenge 2. Write a comment on each line to describe what is happening. 
 # Basically pretend you want to explain this to someone who has never seen it before
 # (You can actually explain this to me on Monday).
@@ -85,7 +92,19 @@ length_of_degree <- function(degree, type = c("lat", "long")) {
 # This documentation will be helpful:
 # https://rdrr.io/github/lmullen/mullenMisc/man/jitter_latlong.html
 
-jitter_latlong(45, km = 50)
+full50 %>% 
+  mutate(lon_jitter = jitter_latlong(full50$lon, type = "long", latitude = full50$lat, km = 50),
+         lat_jitter = jitter_latlong(full50$lat, type = "lat", km = 50))
+# Why doesn't this work?
+view(full50)
+select(full50, lon_jitter, lat_jitter)
+?mutate
+
+### to do:
+# challenges 4 and 5
+# fix the mutate thing ^
+# read article and take notes on figures
+# finish comments on rows for helper function
 
 # Challenge 4. Now plot your jittered lat and long coordinates in leaflet! 
 # Add it your flexdashboard
