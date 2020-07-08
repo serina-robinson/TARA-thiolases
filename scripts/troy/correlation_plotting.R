53b26e7327d7b69b84542f3e2d08453566b20f84
tabiernath <tabiernath@gmail.com>
2020-07-08 17:43:46 -0500

    Update HTML

--- scripts/troy/correlation_plotting.R
+++ scripts/troy/correlation_plotting.R
@@ -1,5 +1,5 @@
 # Install packages
-pacman::p_load("tidyverse", "ggplot2")
+pacman::p_load("tidyverse", "ggplot2", "GGally")
 
 # Set working directory
 setwd("C:/Users/tabie/OneDrive/Documents/GitHub/TARA-thiolases/")
@@ -12,3 +12,13 @@ plot1 <- ggplot(dat) +
   geom_point(aes(x = depth_m, y = temperature, col = polar)) +
   theme_classic()
 plot1
+
+vars <- c("temperature", "temperature_range", "topt")
+subdat <- dat[,colnames(dat) %in% vars]
+ggpairs(subdat) +
+  theme_bw()
+
+
+
+
+
